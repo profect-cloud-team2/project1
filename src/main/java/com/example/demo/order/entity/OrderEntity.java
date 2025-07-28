7a710eb16f3e7177e66bfca0c76e840e424220de
이동규 <idong-gyu@idong-gyuui-MacBookPro.local>
2025-07-28 13:09:49 +0900

    Feat: Order Entity 구현

---
+++ src/main/java/com/example/demo/order/entity/OrderEntity.java
@@ -0,0 +1,56 @@
+package com.example.demo.order.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.example.demo.store.entity.StoreEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "p_order_info")
+public class OrderEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "order_id")
+	private UUID orderId;
+
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "user_id", nullable = false)
+	// private UserEntity userId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id", nullable = false)
+	private StoreEntity storeId;
+
+	@Column(name = "total_price", nullable = false)
+	private int total_price;
+
+	@Column(name = "requestMessage")
+	private String requestMessage;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "order_status", nullable = false)
+	private OrderStatus orderStatus;
+
+	private LocalDateTime createdAt;
+	private UUID createBy;
+
+	private LocalDateTime updatedAt;
+	private UUID updateBy;
+
+	private LocalDateTime deletedAt;
+	private UUID deletedBy;
+
+}
---
+++ src/main/java/com/example/demo/order/entity/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.example.demo.order.entity;
+
+public enum OrderStatus {
+	주문접수, 조리중, 배달중, 배달완료, 주문취소
+}
