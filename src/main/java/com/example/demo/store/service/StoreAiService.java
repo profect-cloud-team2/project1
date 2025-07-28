e428ed5ffe286e70ee4c57ce7275fb6a13e23f27
yejin1201 <hancool65@naver.com>
2025-07-28 14:29:19 +0900

    Feat: env 병합 및 ai api 오류 수정 (#7)
    * Feat: 스토어 등록 기능구현과 디렉토리구조 생성
    
    * Feat: chat gpt API 포함한 자동 설명 기능
    
    * Feat: env 병합 및 ai api 추가
    
    * env 병합 및 ai api오류 수정
    
    * Chore: docker-compose.yml 변수 변경
    
    ---------
    
    Co-authored-by: LeeDongKyu <jjk1491@gmail.com>

--- src/main/java/com/example/demo/store/service/StoreAiService.java
+++ src/main/java/com/example/demo/store/service/StoreAiService.java
@@ -10,18 +10,14 @@ public class StoreAiService {
 
 	private final OpenAiClient openAiClient;
 
-	public String generateAiDescription(String storeName, String category) {
+	public String generateAiDescription(String name, String category) {
 		String prompt = String.format(
 			"가게 이름은 '%s'이고, 음식 카테고리는 '%s'입니다. 이 가게를 간단히 소개해 주세요. " +
-				"답변을 최대한 간결하게 50자 이하로 해주세요.",
-			storeName, category
+				"답변을 최대한 간결하게 50자 이하로 해주세요.답변은 큰따옴표 없이 문장으로 바로 시작해 주세요.",
+			name, category
 		);
 
-		// GPT 호출 대신 테스트용 응답 사용
 		String response = openAiClient.getCompletion(prompt);
-		System.out.println("📌 프롬프트: " + prompt);
-		System.out.println("📌 GPT 응답 (Mock): " + response);
-
 		return extractAnswerOnly(response);
 	}
 
