4ed0d6c1a6d79e044f534a92dc3abc86529ce63d
DK.LEE <jjk1491@gmail.com>
2025-08-05 13:47:37 +0900

    Fix: AiEntity 수정

--- src/main/java/com/example/demo/ai/entity/AiEntity.java
+++ src/main/java/com/example/demo/ai/entity/AiEntity.java
@@ -4,14 +4,12 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -58,6 +56,6 @@ public class AiEntity {
 	@Column(name = "created_at", nullable = false, updatable = false)
 	private LocalDateTime createdAt;
 
-	@Column(name = "created_by", nullable = false)
+	@Column(name = "created_by")
 	private UUID createdBy;
 }
